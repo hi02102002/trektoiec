bab9f711e3cf526db7d3ddf0fd1eca7055c582a3
Võ Hoàng Huy <hoanghuy.dev0210@gmail.com>
2025-12-28 20:50:41 +0700

    Add new favicon images and update existing favicon files
    - Added new favicon images: favicon-32x32.png and favicon-512x512.png.
    - Updated favicon.ico with new PNG data.
    - Removed previous favicon files to streamline assets.

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -64,6 +64,40 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 			links: [
 				...metaLinks,
 				{
+					rel: "icon",
+					type: "image/x-icon",
+					href: "/favicon.ico",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "192x192",
+					href: "/favicon-192x192.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "512x512",
+					href: "/favicon-512x512.png",
+				},
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
+				},
+				{
 					rel: "stylesheet",
 					href: "https://unpkg.com/nprogress@0.2.0/nprogress.css",
 				},
